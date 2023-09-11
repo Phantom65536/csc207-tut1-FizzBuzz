27a4d541d8ee49bf29f97dc8e3baea92c391ab4d
Phantom65536 <phantomsmithhk@gmail.com>
2023-09-11 14:04:57 -0400

    dev algo for multiples

---
+++ src/Multiples.java
@@ -0,0 +1,20 @@
+import static java.lang.Math.min;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+
+        // method 1
+        int lcm = 15;
+        int upper_bound = 1000;
+        System.out.println( (int) ((upper_bound - 1) / x) + (int) ((upper_bound - 1) / y) -
+                (int) ((upper_bound - 1) / lcm));
+
+        // method 2
+        int count = 0;
+        for (int i = min(x, y); i < upper_bound; i++)
+            count = (i % x == 0 || i % y == 0) ? count + 1 : count;
+        System.out.println(count);
+    }
+}
